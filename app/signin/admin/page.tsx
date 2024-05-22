86a9738e14f6ea5a5cb5de7fd34ea8c45b5f6156
Kush <jangidkush1234@gmail.com>
2024-05-22 16:59:53 +0530

    blogs site in next js

---
+++ app/signin/admin/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Admin(){
+    return <div>
+        <div className="text-center border-b">
+        Admin panel
+    </div>
+    </div>
+}
---
+++ app/signin/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Layout({children}:{children:React.ReactNode}){
+return <div className="border-b text-center p-1">
+    20% off for the next 3 days
+    {children}
+</div>
+}
